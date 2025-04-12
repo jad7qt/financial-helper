4e60ed9fb38d287fef363cf1fe3f1860a65e808d
Jared <91357852+jad7qt@users.noreply.github.com>
2025-04-12 16:41:50 -0400

    Initial Project Files

---
+++ financial_helper.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+class FinancialHelper():
+    def __init__(self):
+        # Set dataframe to None until CSV is imported
+        self.df = None
+        
+
+    def upload_csv(self):
+        return
+
+test = FinancialHelper()
