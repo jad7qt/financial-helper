90bf45d7717f02ba863fa52beccbe81953727f92
Jared <91357852+jad7qt@users.noreply.github.com>
2025-04-14 07:26:12 -0400

    Upload CSV function implementation and add Pandas dependency

--- financial_helper.py
+++ financial_helper.py
@@ -1,4 +1,7 @@
 import tkinter as tk
+import pandas as pd
+from tkinter import filedialog
+
 
 class FinancialHelper():
     def __init__(self):
@@ -7,6 +10,21 @@ class FinancialHelper():
         
 
     def upload_csv(self):
+        file_path = filedialog.askopenfilename(
+            title="Select a CSV file",
+            filetypes=[("CSV files", "*.csv")]
+        )
+
+        if file_path:
+            try:
+                self.df = pd.read_csv(file_path)
+            except Exception as e:
+                # TODO: Display no CSV found on page
+                print(f"Failed to load CSV: {e}")
+        else:
+            print("No file path found")
+            # TODO: Display no CSV found on page
         return
 
-test = FinancialHelper()
+if __name__ == "__main__":
+    test = FinancialHelper()
